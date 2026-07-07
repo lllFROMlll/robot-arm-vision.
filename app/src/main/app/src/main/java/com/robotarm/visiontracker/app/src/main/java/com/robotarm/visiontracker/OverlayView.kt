545d111a4de64bfd50dd0caeaf81a8a8e4cafa53
lllFROMlll <thiagofrom@gmail.com>
2026-07-07 04:09:10 -0300

    Create OverlayView.kt

---
+++ app/src/main/app/src/main/java/com/robotarm/visiontracker/app/src/main/java/com/robotarm/visiontracker/OverlayView.kt
@@ -0,0 +1,14 @@
+package com.robotarm.visiontracker
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+class OverlayView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        // TODO (próximas partes): desenhar detecções aqui
+    }
+}
